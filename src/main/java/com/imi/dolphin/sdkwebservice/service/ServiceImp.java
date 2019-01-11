c4d3b0df498376654340671003233aa1558f84fd
Kurnia Sandy Nugraha <42927559+Endorsijahad@users.noreply.github.com>
2019-01-11 14:20:24 +0700

    validatePhoneB4Form 0.0.9

--- src/main/java/com/imi/dolphin/sdkwebservice/service/ServiceImp.java
+++ src/main/java/com/imi/dolphin/sdkwebservice/service/ServiceImp.java
@@ -1867,7 +1867,7 @@ public class ServiceImp implements IService {
             extensionResult.setEntities(clearEntities);
         } 
         if(clearEntities.size() < 1){
-            clearEntities.put("confirm", "mantul");
+            clearEntities.put("confirm", "yes");
             extensionResult.setEntities(clearEntities);
         }
 
